ae7e9261aa112cae8da4975ee214a32f5d9a193f
bing9 <b.luo@outlook.com>
2021-10-02 10:54:00 +0200

    add pytest exceptions

--- price_tracker/src/data/create_products.py
+++ price_tracker/src/data/create_products.py
@@ -1,2 +1,39 @@
+from typing import Union
+
+class ProductList(list):
+    pass
+
 class Product:
+    pass
+
+class SearchTerm:
+    '''Search term is the product name for searching'''
+    implemented_search_domains = ['bol.com']
+
+    def __init__(self, search_term: str, 
+        search_domain: str = 'bol.com',
+        product_list: ProductList = None):
+        self.search_term = search_term
+        self._product_list = product_list
+        self.search_domain = search_domain
+        if self.search_domain not in SearchTerm.implemented_search_domains:
+            raise SearchDomainNotImplementedError(
+                f"Please Use one of the implemented Domains: {SearchTerm.implemented_search_domains}")
+
+    @property
+    def product_list(self):
+        if self._product_list == None:
+            self._product_list = self.get_product_list()
+        return self._product_list
+
+    def get_product_list(self):
+        return ProductList()
+
+    def parse_search_terms(self):
+        return [i.lower() for i in self.search_term.strip().split(' ')]
+
+    def construct_search_url(self):
+        pass
+
+class SearchDomainNotImplementedError(Exception):
     pass
--- price_tracker/tests/test_product_creation.py
+++ price_tracker/tests/test_product_creation.py
@@ -1,4 +1,11 @@
 from price_tracker.src.data.create_products import *
+import pytest
 
-def test_product_class():
-    p = Product()
+def test_search_class():
+    p = SearchTerm('Philips Sonicare')
+    assert isinstance(p.product_list, ProductList)
+    assert p.parse_search_terms() == ['philips', 'sonicare']
+
+def test_search_domain():
+    with pytest.raises(SearchDomainNotImplementedError) as execinfo:
+        p = SearchTerm('Dummy', 'dummy.com')
