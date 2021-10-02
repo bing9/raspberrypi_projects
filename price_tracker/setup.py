53c5deaa13e49bd4b43e4e04ab84734e766182a7
bing9 <b.luo@outlook.com>
2021-10-02 09:45:12 +0200

    Start new project

---
+++ price_tracker/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tracking the Product Prices accross major Distributors in the Netherlands.',
+    author='Bing9',
+    license='MIT',
+)
